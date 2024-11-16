e82d58bd2dd7f531046a6b0b95700beb356ecfbb
Raghu Betina <raghu.betina@chicagobooth.edu>
2024-11-16 17:13:46 +0000

    Indirect association between DirectorResource and ActorResource

--- app/resources/director_resource.rb
+++ app/resources/director_resource.rb
@@ -14,4 +14,18 @@ class DirectorResource < ApplicationResource
 
   # Indirect associations
 
+  has_many :actors do
+    assign_each do |director, actors|
+      actors.select do |a|
+        a.id.in?(director.actors.map(&:id))
+      end
+    end
+  end
+
+
+  filter :actor_id, :integer do
+    eq do |scope, value|
+      scope.eager_load(:actors).where(:roles => {:actor_id => value})
+    end
+  end
 end
