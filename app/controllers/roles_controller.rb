cdf38b40170f176cb99fced00268e700d4beea85
Raghu Betina <raghu.betina@chicagobooth.edu>
2024-11-16 17:15:24 +0000

    Remove controller comments

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,26 +1,21 @@
 class RolesController < ApplicationController
   before_action :set_role, only: [:show, :edit, :update, :destroy]
 
-  # GET /roles
   def index
     @q = Role.ransack(params[:q])
     @roles = @q.result(:distinct => true).includes(:movie, :actor).page(params[:page]).per(10)
   end
 
-  # GET /roles/1
   def show
   end
 
-  # GET /roles/new
   def new
     @role = Role.new
   end
 
-  # GET /roles/1/edit
   def edit
   end
 
-  # POST /roles
   def create
     @role = Role.new(role_params)
 
@@ -36,7 +31,6 @@ class RolesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /roles/1
   def update
     if @role.update(role_params)
       redirect_to @role, notice: 'Role was successfully updated.'
@@ -45,7 +39,6 @@ class RolesController < ApplicationController
     end
   end
 
-  # DELETE /roles/1
   def destroy
     @role.destroy
     message = "Role was successfully deleted."
@@ -58,12 +51,10 @@ class RolesController < ApplicationController
 
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_role
       @role = Role.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def role_params
       params.require(:role).permit(:movie_id, :actor_id, :character_name)
     end
